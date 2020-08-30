8632ef01d85d75ca9eb09a1406b4d30b8ef9aed6
sampan11 <45837589+sampan6108111007@users.noreply.github.com>
2020-08-31 00:57:23 +0700

    Update b1.java

--- java_max_min/b1.java
+++ java_max_min/b1.java
@@ -1,13 +1,13 @@
 public class b1 {
-public static void main(String args[]) {
-		int s1=Integer.parseInt(args[0]);
-		int s2=Integer.parseInt(args[1]);
-		int s3=Integer.parseInt(args[2]);
-		int s4=Integer.parseInt(args[3]);
-			System.out.println( "Plus = "+(s1+ s2 + s3 + s4));
-			System.out.println( "Minus = "+(s1 - s2 - s3- s4));
-			System.out.println( "Multiplied = "+(s1 * s2 * s3 * s4));
-			System.out.println( "Divide = "+(s1 / s2 / s3 / s4));
-			
-		}
+
+  public static void main(String args[]) {
+    int s1 = Integer.parseInt(args[0]);
+    int s2 = Integer.parseInt(args[1]);
+    int s3 = Integer.parseInt(args[2]);
+    int s4 = Integer.parseInt(args[3]);
+    System.out.println("Plus = " + (s1 + s2 + s3 + s4));
+    System.out.println("Minus = " + (s1 - s2 - s3 - s4));
+    System.out.println("Multiplied = " + (s1 * s2 * s3 * s4));
+    System.out.println("Divide = " + (s1 / s2 / s3 / s4));
+  }
 }
