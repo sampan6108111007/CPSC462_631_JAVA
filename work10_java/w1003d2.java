42c83b5cbcef7c8c7e72879c49cec5636233e1a6
sampan11 <45837589+sampan6108111007@users.noreply.github.com>
2020-11-01 23:36:59 +0700

    Add files via upload

---
+++ work10_java/w1003d2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+class w1003d2 {
+  public static void main (String args[]) throws IOException {
+    int i = 0,d;
+    String b;
+    String[] fields;
+    String[] recs = new String[10];
+    String patternStr = ",";
+    
+    FileReader header = new FileReader("h.txt");
+    BufferedReader header1 = new BufferedReader (header);
+    
+    FileReader footer = new FileReader("f.txt");
+    BufferedReader footer1 = new BufferedReader (footer);
+    
+    FileReader fin = new FileReader("subj.txt");
+    BufferedReader bin = new BufferedReader (fin);
+
+    FileOutputStream fout = new FileOutputStream("subj.htm");
+    BufferedOutputStream bout = new BufferedOutputStream(fout);
+    PrintStream pout = new PrintStream(bout);
+   
+    while ((b =  bin.readLine()) != null) {
+      recs[i] = b;
+      i = i + 1;
+    }
+    fin.close();
+    
+    while ((b =  header1.readLine()) != null) {
+        pout.println(b);
+      }header.close();
+
+    pout.println("<body bgcolor=red><table border=1 width=100% >");
+    for(int j=0;j<i;j++) {
+      fields = recs[j].split(patternStr);
+      pout.println("<tr>");
+      pout.print("<td>"+fields[0]+"</td>");
+      pout.print("<td>"+fields[1]+"</td>");
+     // pout.print("<td>"+fields[2]+"</td>");
+      //if (d = Integer.valueOf(fields[2]="A").intValue()){
+       // pout.print("<td>"+fields[3]+"</td>");
+      //}
+     // pout.print("<td>"+d+"</td>");
+      pout.println("</tr>");
+    }fin.close();
+    pout.println("</table></body>");
+
+    while ((b =  footer1.readLine()) != null) {
+      fields = b.split(patternStr);
+      pout.println(b);
+    }footer.close();
+    pout.close();
+  }
+}
---
+++ work10_java/w1004.java
@@ -0,0 +1,60 @@
+import java.io.*;
+class w1004 {
+  public static void main (String args[]) throws IOException {
+    int i = 0,d;
+    String b;
+    String[] fields;
+    String[] recs = new String[10];
+    String patternStr = ",";
+    
+    FileReader header = new FileReader("h.txt");
+    BufferedReader header1 = new BufferedReader (header);
+    
+    FileReader footer = new FileReader("f.txt");
+    BufferedReader footer1 = new BufferedReader (footer);
+    
+    FileReader fin = new FileReader("gread.txt");
+    BufferedReader bin = new BufferedReader (fin);
+
+    FileOutputStream fout = new FileOutputStream("gread.htm");
+    BufferedOutputStream bout = new BufferedOutputStream(fout);
+    PrintStream pout = new PrintStream(bout);
+   
+    while ((b =  bin.readLine()) != null) {
+      recs[i] = b;
+      i = i + 1;
+    }
+    fin.close();
+    
+    while ((b =  header1.readLine()) != null) {
+        pout.println(b);
+      }header.close();
+
+    pout.println("<body bgcolor=red><table border=1 width=100% >");
+    for(int j=0;j<i;j++) {
+      fields = recs[j].split(patternStr);
+      pout.println("<tr>");
+      pout.print("<td>"+fields[0]+"</td>");
+      pout.print("<td>"+fields[1]+"</td>");
+      pout.print("<td>"+fields[2]+"</td>");
+      if(fields[2].equals("A")){pout.println("<td>"+"4"+"</td>");}
+      else if(fields[2].equals("B")){pout.println("<td>"+"3"+"</td>");}
+      else if(fields[2].equals("C")){pout.println("<td>"+"2"+"</td>");}
+      else if(fields[2].equals("D")){pout.println("<td>"+"1"+"</td>");}
+      else {pout.println("<td>"+"0"+"</td>");}
+     // pout.print("<td>"+fields[2]+"</td>");
+      //if (d = Integer.valueOf(fields[2]="A").intValue()){
+       // pout.print("<td>"+fields[3]+"</td>");
+      //}
+     // pout.print("<td>"+d+"</td>");
+      pout.println("</tr>");
+    }fin.close();
+    pout.println("</table></body>");
+
+    while ((b =  footer1.readLine()) != null) {
+      fields = b.split(patternStr);
+      pout.println(b);
+    }footer.close();
+    pout.close();
+  }
+}
